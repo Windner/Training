96d07a9911dc73ede07da69b6431e5440401ecd7
candice_feng <candice_feng@htc.com>
2018-01-29 15:40:32 +0800

    Lesson 1

---
+++ app/src/main/java/com/example/candice_feng/training/Lesson1.java
@@ -0,0 +1,35 @@
+package com.example.candice_feng.training;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+public class Lesson1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Create a RelativeLayout
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+
+        //Create an imageViewer
+        ImageView imgView = new ImageView(this);
+
+        //Set ImageViewer resource
+        imgView.setImageResource(R.drawable.ic_launcher_foreground);
+
+        //Set layout parameter
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        lp.addRule(RelativeLayout.CENTER_IN_PARENT);
+
+        //Add parameter to imageViewer
+        imgView.setLayoutParams(lp);
+
+        //Add ImageViewer to RelativeLayout
+        relativeLayout.addView(imgView);
+
+        //Set to content view
+        setContentView(relativeLayout);
+    }
+}
--- app/src/main/java/com/example/candice_feng/training/MainActivity.java
+++ app/src/main/java/com/example/candice_feng/training/MainActivity.java
@@ -1,21 +1,27 @@
 package com.example.candice_feng.training;
 
-import android.net.Uri;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LinearLayout linearLayout = new LinearLayout(this);
-        ImageView imgView = new ImageView(this);
-        //imgView.setImageURI(Uri.parse("https://lh3.googleusercontent.com/0-BzaWtxoAnsBjQ_wzUcKxyF07XE7v2Kkg1ogPVUdzmQpvaz118uHQEGU6BdtzJuzfo=h1264"));
-        //imgView.setImageDrawable(getDrawable());
-        linearLayout.addView(imgView);
-        setContentView(linearLayout);
+        //Set to content view
+        setContentView(R.layout.activity_main);
+        final Button lesson1_btn = findViewById(R.id.lesson_one_btn);
+        lesson1_btn.setText(R.string.lesson_one);
+        lesson1_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Lesson1.class));
+            }
+        });
     }
+
+
 }
