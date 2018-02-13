3ca866603af81a694499d4ee562e3769838a63e5
candice_feng <candice_feng@htc.com>
2018-02-13 18:45:29 +0800

    Add Lesson 5 HW 8.3 init

--- app/src/main/java/com/example/candice_feng/training/Lesson5_sub/HomeWork_8_3.java
+++ app/src/main/java/com/example/candice_feng/training/Lesson5_sub/HomeWork_8_3.java
@@ -2,6 +2,8 @@ package com.example.candice_feng.training.Lesson5_sub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.candice_feng.training.R;
 
@@ -11,5 +13,13 @@ public class HomeWork_8_3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_work_8_3);
+
+        Button downloadBtn = findViewById(R.id.download_btn);
+        downloadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
